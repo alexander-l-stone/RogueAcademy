c7584326ce8d74aa78bf8d910476be8d3d708c96
Richard Nicholson <twilly0@gmail.com>
2020-03-08 22:45:59 -0700

    Refactored GrammarRule to be self-contained. Added tests.

--- source/grammar/GrammarRule.py
+++ source/grammar/GrammarRule.py
@@ -1,7 +1,30 @@
-from typing import List
+from typing import List,Dict
+import random
 
 class GrammarRule:
 
-    def __init__(self, parent:GrammarRule=None, selections:List[List]=[]):
-        self.parent:GrammarRule = parent # rule
+    def __init__(self, selections:List[List]=[]):
         self.selections:List[List] = selections # list rule or typestring
+
+    @staticmethod
+    def generate(rule:GrammarRule):
+        if rule == None:
+            return None
+        arr:List = [rule]
+
+        for i,r in enumerate(arr):
+            # pop the rule
+            arr.remove(i)
+            # clone the template object
+            if r is not GrammarRule:
+                arr.insert(i,r.clone())
+                continue
+            # pick an option for this symbol
+            rand = random.randrange(0, len(rule.selections))
+            selection = rule.selections[rand]
+            # add the chosen selection
+            # go from the back so they end up in order
+            for child in selection.reversed():
+                arr.insert(i, child)
+
+        return arr
