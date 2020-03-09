4b88df6d84e85635dc48672b21a9ad9a1bb030f6
Alexander Stone <aloecend@gmail.com>
2020-03-09 00:54:50 -0400

    Bugfixes

--- source/grammar/GrammarRule.py
+++ source/grammar/GrammarRule.py
@@ -1,5 +1,6 @@
 from typing import List,Dict
 import random
+import copy
 
 class GrammarRule:
 
@@ -7,17 +8,17 @@ class GrammarRule:
         self.selections:List[List] = selections # list rule or typestring
 
     @staticmethod
-    def generate(rule:GrammarRule):
+    def generate(rule): #rule must be of type Grammar Rule
         if rule == None:
             return None
         arr:List = [rule]
 
         for i,r in enumerate(arr):
             # pop the rule
-            arr.remove(i)
+            arr.pop(i)
             # clone the template object
             if r is not GrammarRule:
-                arr.insert(i,r.clone())
+                arr.insert(i,copy.deepcopy(r))
                 continue
             # pick an option for this symbol
             rand = random.randrange(0, len(rule.selections))
