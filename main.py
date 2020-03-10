d56c8605679152bd88988bb0fbbff0da500e4bd1
Alexander Stone <aloecend@gmail.com>
2020-03-10 18:48:29 -0200

    WIP: Add missing __init__'s. Moved most of the code into a game class.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import tcod
 import tcod.event
 
-from source.entity.drawableEntity import DrawableEntity
-from source.entity.player import Player
-from source.area.area import Area
+from source.game import Game
 
 # Setup the font.
 tcod.console_set_custom_font(
@@ -11,28 +9,13 @@ tcod.console_set_custom_font(
     tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE,)
 
 #test object
-player_entity:Player = Player(0, 24, 24, '@', (255, 255, 255))
+
 SCREEN_HEIGHT:int = 50
 SCREEN_WIDTH:int = 50
 
-area:Area = Area(2,100,100)
-
-for x in range(area.x_length):
-    for y in range(area.y_length):
-        if (x+1)%5 == 0 or (y+1)%5 == 0:
-            area.map[0,x,y] = False
-
-area.objdict[(player_entity.z, player_entity.x, player_entity.y)] = player_entity
-for x in range(player_entity.x-5, player_entity.x-4):
-    for y in range(player_entity.y-5, player_entity.y-4):
-        area.objdict[(player_entity.z, x, y)] = DrawableEntity(player_entity.z, x, y, 'K', (x*5,abs(x*10 - y*10),y*5))
+game = Game()
+game.generate_school()
 
 # Initialize the root console in a context.
-with tcod.console_init_root(SCREEN_HEIGHT, SCREEN_WIDTH, order="F", vsync=False) as root_console:
-    while True:
-        area.draw(player_entity.z, player_entity.x, player_entity.y, SCREEN_WIDTH, SCREEN_HEIGHT)
-        tcod.console_flush()  # Show the console.
-        for event in tcod.event.wait():
-            if event.type == "QUIT":
-                raise SystemExit()
+game.game_loop()
     # The libtcod window will be closed at the end of this with-block.
---
+++ source/handlers/inputHandler.py
@@ -0,0 +1,10 @@
+import tcod.event
+
+#TODO: Figure out a good way of doing input handling.
+
+class InputHandler:
+    def __init__(self):
+        pass
+    
+    def handle_keypress(self, event):
+        print(event)
