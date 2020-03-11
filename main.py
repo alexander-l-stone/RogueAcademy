abd67b3037ea226710bc691610694523f567e3c9
Alexander Stone <aloecend@gmail.com>
2020-03-10 23:46:54 -0400

    Repush of main.py

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ import tcod.event
 
 from source.game import Game
 
-# Setup the font.
-tcod.console_set_custom_font(
-    "arial12x12.png",
-    tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE,)
-
-#test object
-
-SCREEN_HEIGHT:int = 50
-SCREEN_WIDTH:int = 50
-
 game = Game()
 game.generate_school()
 
