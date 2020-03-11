f9cf6206638f89ea2b8ea85813f46a7d73289c29
Alexander Stone <aloecend@gmail.com>
2020-03-11 00:28:27 -0400

    Updated main with the correct conditional statement.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import tcod
 import tcod.event
 
 from source.game import Game
+if __name__ == "__main__":
+    game = Game()
+    game.generate_school()
 
-game = Game()
-game.generate_school()
-
-# Initialize the root console in a context.
-game.game_loop()
-# The libtcod window will be closed at the end of this with-block.
+    # Initialize the root console in a context.
+    game.game_loop()
+    # The libtcod window will be closed at the end of this with-block.
