aaf87397f54ba06a691d25c6b3ecac523aaff336
Richard Nicholson <twilly0@gmail.com>
2020-03-10 21:42:12 -0700

    Updated grammar parsing to include nodes which create variables instead of outputting.

--- source/grammar/GrammarRule.py
+++ source/grammar/GrammarRule.py
@@ -4,31 +4,68 @@ import copy
 
 class GrammarRule:
 
-    # TODO add code to store results as variables for use in the remainder of generation
     # TODO consider adding function hooks to rules for self-population
 
-    def __init__(self, selections:List[List]=[]):
+    def __init__(self, selections:List[List]=[], assignVar:str=None):
         self.selections:List[List] = selections # list rule or typestring
+        self.assignVar = assignVar
 
     @staticmethod
-    def generate(rule): #rule must be of type Grammar Rule
-        if rule == None:
+    def generate(root): # must be type GrammarRule; can't use typing because GrammarRule is not yet defined
+        if root == None or type(root) is not GrammarRule:
             return None
-        arr:List = [rule]
+        stack:List = [root]
+        output:List = []
+        variables = {}    
 
-        for i,r in enumerate(arr):
-            # pop the rule
-            arr.pop(i)
-            # clone the template object
-            if type(r) is not GrammarRule:
-                arr.insert(i,copy.deepcopy(r))
-                continue
-            # pick an option for this symbol
-            rand = random.randrange(0, len(rule.selections))
-            selection = rule.selections[rand]
-            # add the chosen selection
-            # go from the back so they end up in order
-            for child in selection[::-1]:
-                arr.insert(i, child)
+        while len(stack) > 0:
+            # pop the element
+            elem = stack.pop()
 
-        return arr
+            if type(elem) is GrammarRule:
+                # pick an option for this symbol
+                rand = random.randrange(0, len(elem.selections))
+                selection = elem.selections[rand]
+                # assign to a variable if necessary
+                if elem.assignVar:
+                    variables[elem.assignVar] = selection
+                else:
+                    # add the chosen selection
+                    # go from the back so they end up in order
+                    for child in selection[::-1]:
+                        stack.append(child)
+            elif type(elem) is GrammarVariable:
+                # add the variable's value(s)
+                # go from the back so they end up in order
+                value = variables.get(elem)
+                if value:
+                    for child in value[::-1]:
+                        stack.append(child)
+                else:
+                    stack.append(f"Variable {elem} not found")
+            else:
+                # copy the template object
+                output.append(copy.deepcopy(elem))
+
+        return output
+    
+    def __str__(self):
+        return f"<{self.selections}, {self.assignVar}>"
+
+class GrammarVariable:
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self,other):
+        if other is GrammarVariable:
+            return self.name == other.name
+        return self.name == other
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
+
+    def __hash__(self):
+        return self.name.__hash__()
---
+++ test/test_grammar.py
@@ -0,0 +1,53 @@
+import pytest
+from source.grammar.GrammarRule import GrammarRule
+from source.grammar.GrammarRule import GrammarVariable
+
+def test_can_instantiate_rule():
+    assert GrammarRule
+    rule = GrammarRule()
+    assert GrammarRule is type(rule)
+
+def test_can_instantiate_rule_with_args():
+    selections = [["a"],["b"]]
+    rule = GrammarRule(selections)
+    assert selections == rule.selections
+
+def test_can_generate_rule():
+    selections = [["a"],["b"]]
+    rule = GrammarRule(selections)
+    result = GrammarRule.generate(rule)
+    assert selections[0] == result or selections[1] == result
+    assert selections[0] is not result
+    assert selections[1] is not result
+
+    selections = [["a","c"],["b","d"]]
+    rule = GrammarRule(selections)
+    result = GrammarRule.generate(rule)
+    assert selections[0] == result or selections[1] == result
+    assert selections[0] is not result
+    assert selections[1] is not result
+
+def test_can_generate_recursive():
+    rule_leaf = GrammarRule([["text"]])
+    rule_branch = GrammarRule([[rule_leaf]])
+    rule_root = GrammarRule([[rule_branch]])
+    result = GrammarRule.generate(rule_root)
+    assert ["text"] == result
+
+def test_variable():
+    var1 = GrammarVariable("var1")
+    str1 = "var1"
+    assert var1 == str1
+    assert var1.__hash__() == str1.__hash__()
+
+def test_use_variable():
+    str_varname = "var_1"
+    str_value = "populate"
+    rule_assign = GrammarRule([[str_value]], str_varname)
+    print(f"rule_assign = {rule_assign}")
+    var_recall_bad = GrammarVariable("var_garbage")
+    var_recall_good = GrammarVariable(str_varname)
+    root = GrammarRule([[rule_assign, var_recall_bad, var_recall_good]])
+    result = GrammarRule.generate(root)
+    assert str_varname == var_recall_good
+    assert [None, str_value] == result
