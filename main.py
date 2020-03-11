07b05da0f2840622f2df872231dd7f1166dd8318
Alexander Stone <aloecend@gmail.com>
2020-03-11 00:27:36 -0400

    Example of multiprocessing now in comemnts.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import tcod.event
 
 from source.game import Game
 
-game = Game()
-game.generate_school()
 
-# Initialize the root console in a context.
-game.game_loop()
-# The libtcod window will be closed at the end of this with-block.
+if __name__ == '__main__':
+    #Make a Game
+    game = Game()
+    #Generate a School
+    game.generate_school()
+    #Run the game
+    game.game_loop()
--- source/game.py
+++ source/game.py
@@ -1,5 +1,6 @@
 import tcod
 import tcod.event
+from multiprocessing import Process
 
 from source.area.area import Area
 from source.handlers.inputHandler import InputHandler
@@ -30,13 +31,23 @@ class Game:
                 self.curr_area.add_object(DrawableEntity(self.player.z, x, y, 'K', (x*5, abs(x*10 - y*10), y*5)))
 
     def render(self):
+        """
+            Renders the game to the console
+        """
         self.curr_area.draw(self.player.z, self.player.x,self.player.y, self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
         tcod.console_flush()  # Show the console.
     
     def game_loop(self):
+        """
+            Main loop that runs the game
+        """
         with tcod.console_init_root(self.SCREEN_HEIGHT, self.SCREEN_WIDTH, order="F", vsync=False) as root_console:
             while not tcod.console_is_window_closed():
                 root_console.clear()
+                # Example of multiprocessing
+                #p = Process(target=self.render())
+                #p.start()
+                #p.join
                 self.render()
                 for event in tcod.event.wait():
                     if event.type == "KEYDOWN":
