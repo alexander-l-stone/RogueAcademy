3f67b4646208cc53427c8ec5b42ca212a32cfc4a
Alexander Stone <aloecend@gmail.com>
2020-03-10 21:44:07 -0400

    Added support for wasd and numpad. Fixed keys not handled crashing the program.

--- source/handlers/inputHandler.py
+++ source/handlers/inputHandler.py
@@ -7,11 +7,22 @@ class InputHandler:
         pass
     
     def handle_keypress(self, event):
-        if(event.sym == tcod.event.K_UP):
+        if(event.sym == tcod.event.K_UP or event.sym == tcod.event.K_w or event.sym == tcod.event.K_KP_8):
             return {"type": "move", "value": (0, 0, -1)}
-        elif(event.sym == tcod.event.K_DOWN):
+        elif(event.sym == tcod.event.K_DOWN or event.sym == tcod.event.K_s or event.sym == tcod.event.K_KP_2):
             return {"type": "move", "value": (0, 0, 1)}
-        elif(event.sym == tcod.event.K_LEFT):
+        elif(event.sym == tcod.event.K_LEFT or event.sym == tcod.event.K_a or event.sym == tcod.event.K_KP_4):
             return {"type": "move", "value": (0, -1, 0)}
-        elif(event.sym == tcod.event.K_RIGHT):
+        elif(event.sym == tcod.event.K_RIGHT or event.sym == tcod.event.K_d or event.sym == tcod.event.K_KP_6):
             return {"type": "move", "value": (0, 1, 0)}
+        elif(event.sym == tcod.event.K_KP_9):
+            return {"type": "move", "value": (0, 1, -1)}
+        elif(event.sym == tcod.event.K_KP_7):
+            return {"type": "move", "value": (0, -1, -1)}
+        elif(event.sym == tcod.event.K_KP_1):
+            return {"type": "move", "value": (0, -1, 1)}
+        elif(event.sym == tcod.event.K_KP_3):
+            return {"type": "move", "value": (0, 1, 1)}
+        else:
+            print(event)
+            return {"type": "none"}
